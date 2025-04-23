9e2e14fcc1c615d801235ce086fd992912de4f02
1234Rowdybaby <167402968+1234Rowdybaby@users.noreply.github.com>
2025-04-23 13:39:07 +0530

    Update info.py

--- info.py
+++ info.py
@@ -25,7 +25,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 USERNAME = environ.get('USERNAME', 'https://t.me/Sandymaiwait')
 LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002433610423'))
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002694840394').split()]
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://Sandy1:Sandy1@cluster0.ox0rilw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://koolscott5:U26geFv5dNH5HJ9u@movie.khl6kij.mongodb.net/?retryWrites=true&w=majority&appName=movie")
 DATABASE_URI2 = environ.get('DATABASE_URI2', "mongodb+srv://koolscott5:U26geFv5dNH5HJ9u@movie.khl6kij.mongodb.net/?retryWrites=true&w=majority&appName=movie")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Rahul")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Rahul')
